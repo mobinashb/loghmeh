7171553728ee527bf2e83696ac16352073df616f
Mobina <shbmobina@gmail.com>
2020-06-10 22:41:14 +0430

    Change server URL

--- src/Constants/Constants.js
+++ src/Constants/Constants.js
@@ -1,2 +1,2 @@
-export const SERVER_URI = "http://localhost:8080/v1";
+export const SERVER_URI = "http://ie.etuts.ir:32013/v1";
 export const CLIENT_ID = "568052004069-80istmajegjol2dd97o4mt67imfemads.apps.googleusercontent.com";
