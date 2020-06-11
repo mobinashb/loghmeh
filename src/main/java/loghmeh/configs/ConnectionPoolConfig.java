b018180ed91564c2c1cef436b33d282921b1d3d3
omigo2000 <omid.bodaghi79@gmail.com>
2020-06-12 02:52:58 +0430

    Update

--- src/main/java/loghmeh/configs/ConnectionPoolConfig.java
+++ src/main/java/loghmeh/configs/ConnectionPoolConfig.java
@@ -2,7 +2,8 @@ package loghmeh.configs;
 
 public class ConnectionPoolConfig {
     public static final String DRIVER_CLASS_NAME = "com.mysql.cj.jdbc.Driver";
-    public static final String DATABASE_URL = "jdbc:mysql://loghmeh-mysql:3306/Loghmeh?useUnicode=true&characterEncoding=UTF-8";
+    public static final String DATABASE_URL = "jdbc:mysql://loghmeh-mysql:3306/Loghmeh";
+//    public static final String DATABASE_URL = "jdbc:mysql://loghmeh-mysql:3306/Loghmeh?useUnicode=true&characterEncoding=UTF-8";
 //    public static final String DATABASE_URL = "jdbc:mysql://localhost:3306/Loghmeh?useUnicode=true&characterEncoding=UTF-8";
     public static final String DATABASE_USERNAME = "root";
     public static final String DATABASE_PASSWORD = "2213";
